499ed03e661a7dd37e6dce99daf1fefa84c2a037
Wasif Tai <muhammadwasiftai@gmail.com>
2025-10-27 21:51:23 +0500

    first commit

---
+++ app.js
@@ -0,0 +1,35 @@
+alert("Welcome To Wasif Website😊");
+
+console.log("Website loaded successfully.");
+document.querySelector(".Task-1").addEventListener("click", function () {
+    alert("Welcome To Wasif Website😊");
+});
+
+console.log("Website loaded successfully.");
+document.querySelector(".Task-2").addEventListener("click", function () {
+    alert("Error! Please enter a valid password.");
+});
+
+document.querySelector(".Task-3").addEventListener("click", function () {
+    alert("Welcome to JS Land...\nHappy Coding!");
+});
+
+document.querySelector(".Task-4").addEventListener("click", function () {
+    alert("Welcome to JS Land...");
+});
+
+document.querySelector(".Task-4").addEventListener("click", function () {
+    alert("Happy Coding!");
+});
+
+document.querySelector(".Task-5").addEventListener("click", function () {
+    alert("Hello... I can run Javascript through my web browser's console");
+});
+
+document.querySelector(".Task-6").addEventListener("click", function () {
+    alert("Here, Alert is working😂, I didn't knew what to do here");
+});
+
+document.querySelector(".Task-7").addEventListener("click", function () {
+    alert("Placed Script Everywhere, Head, Start Of Body, & End Of Body, So This Proves I Practiced Task 7, You Can See It On My Repo, Thats Why You Were Clicking Ok 3 times For the 1st Alert😂 As There Are 3 scripts present in html file so the bowser is runing JS 3 times");
+});
